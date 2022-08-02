41801745bf468dfb3b3b77b8c0b800753822ee7f
SomjaniNileshMulchand <somjaninilesh@gmail.com>
2022-08-02 06:11:21 -0400

    Update

--- JS.js
+++ JS.js
@@ -1,11 +1,3 @@
-/*var BOY = confirm('ARE YOU 18 ABOVE');
-console.log(BOY);
-
-var boy = confirm('YOU ARE BOY');
-console.log(boy);*/
-
-
-
 var age = prompt('Enter you are age');
 
 if (age > 18){
