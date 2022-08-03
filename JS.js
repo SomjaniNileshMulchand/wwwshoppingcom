24d9ca5044f85bba2e842b7cec27744b78558d96
SomjaniNileshMulchand <somjaninilesh@gmail.com>
2022-08-03 16:59:35 -0400

    Update

--- JS.js
+++ JS.js
@@ -1,7 +1,7 @@
-var age = prompt('Enter you are age');
+var age = prompt ('Enter you are age');
 
 if (age > 18){
-    console.log('are you 18 above');
+    document.write('are you 18 above');
 }else{
-    console.log('are you not 18 above');
+    document.write('are you not 18 above');
 }
